932eee98803c461510f4f870f2d7a33baee33467
irinelko <irinelko@gmail.com>
2020-12-15 10:14:41 +0100

    stable

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import logo from './ss-w.png';
 import './App.css';
 
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p>Snapshot
           Irinel Tapalaga
         </p>
         <a
-          className="App-link"
+          className="LinkedIN-link"
           href="https://www.linkedin.com/in/irinel-tapalaga/"
           target="_blank"
           rel="noopener noreferrer"
         >
           LinkedIN
         </a>
+        <a
+          className="Git-link"
+          href="https://github.com/irinelko/RegressionTestGrail"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GitHub
+        </a>
       </header>
+      <AmplifySignOut />
     </div>
   );
 }
